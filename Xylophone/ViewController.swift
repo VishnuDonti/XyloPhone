f8becbf8dd574f60f60550b1e04a704737edd6b1
vishnuvardhan.donti@gmail.com <vishnuvardhan.donti@gmail.com>
2023-07-01 21:26:41 +0400

    XyloPhone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        playSound(button1: (sender.titleLabel?.text)!);
+    }
+    
+    func playSound(button1: String){
+        let url = Bundle.main.url(forResource: button1, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
     
 
 }
